45da8c17a99b39d602dd9e33304f3ec35d98c2d8
joey8angelo <joey8angelo@gmail.com>
2023-09-07 14:24:59 -0700

    find/group/test with match start and end behavior - test does not work properly

--- src/match.cpp
+++ src/match.cpp
@@ -2,10 +2,10 @@
 
 /*
     Find the longest match from the left. NOT the longest match in the string
+    If we are searching the back of the string because of a $
+    Then j becomes str.size()-j-1 to match from the end
 */
 std::pair<int, std::string> Regex::find(std::string str){
-    if(reversed)
-        return Rfind(str);
     // if the string must be matched from the start only iterate one time
     int cmp = matchStart ? 1 : str.size();
     for(int i = 0; i < cmp; i++){
@@ -19,9 +19,9 @@ std::pair<int, std::string> Regex::find(std::string str){
         
         for(int j = i; j < str.size(); j++){
             if(current.find(nfaAcc) != current.end())
-                lastAcc = j;
+                lastAcc = reversed ? str.size()-j : j;
             for(int p : current){
-                if(nfa[p]->c == str[j] && nfa[p]->out1){
+                if(nfa[p]->c == str[reversed ? str.size()-j-1 : j] && nfa[p]->out1){
                     next.insert(nfa[p]->out1->ID);
                     temp = epsilonClosure(nfa[p]->out1->ID);
                     for(int v : temp){next.insert(v);}
@@ -29,11 +29,15 @@ std::pair<int, std::string> Regex::find(std::string str){
             }
             // if next set contains the accept state and the string is completely used
             if(next.find(nfaAcc) != next.end() && j == str.size()-1){
+                if(reversed)
+                    return std::make_pair(str.size()-j-1, str.substr(str.size()-j-1, str.size()-(str.size()-j-1)));
                 return std::make_pair(i, str.substr(i, j-i+1));
             }
             // if the next set is empty no possible paths can be taken
             // if an accept state was previously seen return from that position
             if(!next.size() && lastAcc != -1 && !matchEnd){
+                if(reversed)
+                    return std::make_pair(lastAcc, str.substr(lastAcc, str.size()-lastAcc));
                 return std::make_pair(i, str.substr(i, lastAcc-i));
             }
             // continue searching the string for a match if next is empty
@@ -56,62 +60,119 @@ std::pair<int, std::string> Regex::find(std::string str){
 }
 
 /*
-    Almost exactly the same as the normal find with some key differences
-    There is no need to match from anywhere but the end so no outer loop
-    Searches from the back of the string
-    How the substring is computed is slightly different
+    Returns true if any sequence of characters leads to an accept state
+    Augments the nfa with .* at the start to nondeterministically find the accept state
+    if its not necessary to match from the start of the string
 */
-std::pair<int, std::string> Regex::Rfind(std::string str){
+bool Regex::test(std::string str){
+    int augStart;
+    int augAcc;
+    if(matchStart){
+        augStart = nfaStart;
+    }
+    else{
+        Regex::NFAState* augS = new Regex::NFAState(-1, &nfa);
+        Regex::NFAState* a = new Regex::NFAState("^\n\r", -2, &nfa);
+        Regex::NFAState* b = new Regex::NFAState(-3, &nfa);
+
+        a->out1 = augS;
+        a->out2 = b;
+        augS->out1 = b;
+        b->out2 = augS;
+        b->out1 = nfa[nfaStart];
+        augStart = -1;
+    }
+
+    if(matchEnd){
+        augAcc = nfaAcc;
+    }
+    else{
+        Regex::NFAState* a = new Regex::NFAState(-4, &nfa);
+        Regex::NFAState* b = new Regex::NFAState("^\n\r", -5, &nfa);
+        Regex::NFAState* augA = new Regex::NFAState(-6, &nfa);
+
+        nfa[nfaAcc]->out1 = a;
+        a->out1 = b;
+        a->out2 = augA;
+        b->out1 = augA;
+        augA->out2 = b;
+        augAcc = -6;
+    }
     // step over the NFA, keep a set of all the states currently in, and compute the next step
     std::unordered_set<int> current;
     std::unordered_set<int> next;
-    current.insert(nfaStart);
-    std::unordered_set<int> temp = epsilonClosure(nfaStart);
+    current.insert(augStart);
+    std::unordered_set<int> temp = epsilonClosure(augStart);
     for(int v : temp){current.insert(v);}
-    int lastAcc = -1;
-    
-    for(int j = str.size()-1; j >= 0; j--){
+        
+    for(int j = 0; j < str.size(); j++){
+        if(current.find(augAcc) != current.end()){
+            if(augStart != nfaStart){
+                delete nfa[-1];
+                nfa.erase(-1);
+            }
+            if(augAcc != nfaAcc){
+                nfa[nfaAcc]->out1 = nullptr;
+                delete nfa[-2];
+                nfa.erase(-2);
+            }
+            return true;
+        }
         for(int p : current){
-            if(nfa[p]->c == str[j] && nfa[p]->out1){
+            if(nfa[p]->c == str[reversed ? str.size()-j-1 : j] && nfa[p]->out1){
                 next.insert(nfa[p]->out1->ID);
                 temp = epsilonClosure(nfa[p]->out1->ID);
                 for(int v : temp){next.insert(v);}
             }
         }
         // if next set contains the accept state and the string is completely used
-        if(next.find(nfaAcc) != next.end() && j == 0){
-            return std::make_pair(j, str.substr(j, str.size()-j));
-        }
-        // if the next set is empty no possible paths can be taken
-        // if an accept state was previously seen return from that position
-        if(!next.size() && lastAcc != -1 && !matchEnd){
-            return std::make_pair(lastAcc, str.substr(lastAcc, str.size()-lastAcc));
+        if(next.find(augAcc) != next.end() && j == str.size()-1){
+            if(augStart != nfaStart){
+                delete nfa[-1];
+                nfa.erase(-1);
+            }
+            if(augAcc != nfaAcc){
+                nfa[nfaAcc]->out1 = nullptr;
+                delete nfa[-2];
+                nfa.erase(-2);
+            }
+            return true;
         }
         // continue searching the string for a match if next is empty
         if(!next.size()){
             break;
         }
-        if(next.find(nfaAcc) != next.end())
-            lastAcc = j;
         current = next;
         next.clear();
     }
     // check if the start state is also an accept state via epsilon
     std::unordered_set<int> t;
-    t.insert(nfaStart);
-    std::unordered_set<int> t1 = epsilonClosure(nfaStart);
+    t.insert(augStart);
+    std::unordered_set<int> t1 = epsilonClosure(augStart);
     for(int v : t1){t.insert(v);}
-    if(t.find(nfaAcc) != t.end())
-        return std::make_pair(0, "");
-    // if not, no match
-    return std::make_pair(-1, "");
-}
-
-/*
-    Returns true if there is a match in the string
-*/
-bool Regex::test(std::string str){
-    return find(str).first == -1 ? false : true; 
+    if(t.find(nfaAcc) != t.end()){
+        if(augStart != nfaStart){
+            delete nfa[-1];
+            nfa.erase(-1);
+        }
+        if(augAcc != nfaAcc){
+            nfa[nfaAcc]->out1 = nullptr;
+            delete nfa[-2];
+            nfa.erase(-2);
+        }
+        return true;
+    }
+    
+    if(augStart != nfaStart){
+        delete nfa[-1];
+        nfa.erase(-1);
+    }
+    if(augAcc != nfaAcc){
+        nfa[nfaAcc]->out1 = nullptr;
+        delete nfa[-2];
+        nfa.erase(-2);
+    }
+    return false;
 }
 
 /*
@@ -131,23 +192,29 @@ std::vector<std::pair<int, std::string>> Regex::group(std::string str){
         
         for(int j = i; j < str.size(); j++){
             if(current.find(nfaAcc) != current.end())
-                lastAcc = j;
+                lastAcc = reversed ? str.size()-j : j;
             for(int p : current){
-                if(nfa[p]->c == str[j] && nfa[p]->out1){
+                if(nfa[p]->c == str[reversed ? str.size()-j-1 : j] && nfa[p]->out1){
                     next.insert(nfa[p]->out1->ID);
                     temp = epsilonClosure(nfa[p]->out1->ID);
                     for(int v : temp){next.insert(v);}
                 }
             }
             if(next.find(nfaAcc) != next.end() && j == str.size()-1){
-                res.push_back(std::make_pair(i, str.substr(i, j-i+1)));
+                if(reversed)
+                    res.push_back(std::make_pair(str.size()-j-1, str.substr(str.size()-j-1, str.size()-(str.size()-j-1))));
+                else
+                    res.push_back(std::make_pair(i, str.substr(i, j-i+1)));
                 if(j == str.size()-1)
                     j++;
                 i=j-1;
                 break;
             }
             else if(!next.size() && lastAcc != -1 && !matchEnd){
-                res.push_back(std::make_pair(i, str.substr(i, lastAcc-i)));
+                if(reversed)
+                    res.push_back(std::make_pair(lastAcc, str.substr(lastAcc, str.size()-lastAcc)));
+                else
+                    res.push_back(std::make_pair(i, str.substr(i, lastAcc-i)));
                 if(j == str.size()-1)
                     j++;
                 i=j-1;
